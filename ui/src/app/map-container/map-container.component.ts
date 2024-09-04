5c373715fc80c97bc73b72cd3c287bd22a328382
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-09-04 18:24:43 +0530

    leaflet maps

--- ui/src/app/map-container/map-container.component.ts
+++ ui/src/app/map-container/map-container.component.ts
@@ -1,12 +1,24 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { latLng, tileLayer } from 'leaflet';
 import { NgxPlaceholderComponent } from 'ngx-placeholder';
 
 @Component({
   selector: 'app-map-container',
   standalone: true,
-  imports: [NgxPlaceholderComponent],
+  imports: [NgxPlaceholderComponent, LeafletModule],
   templateUrl: './map-container.component.html',
   styleUrl: './map-container.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class MapContainerComponent {}
+export class MapContainerComponent {
+  public options: any = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+      }),
+    ],
+    zoom: 10,
+    center: latLng(18.5204, 73.8567),
+  };
+}
