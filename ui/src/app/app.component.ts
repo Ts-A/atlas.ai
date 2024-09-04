83837420c982ddd53ba6ebc280f2c7392244a53e
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-09-04 17:54:48 +0530

    initial ui layout

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { RouterOutlet } from '@angular/router';
+import { NgxPlaceholderComponent } from 'ngx-placeholder';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxPlaceholderComponent, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
