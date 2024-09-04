b4244afc251ac7abf2bcddb7de8973e299ffb68b
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-09-04 18:02:52 +0530

    footer

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { RouterOutlet } from '@angular/router';
 import { NgxPlaceholderComponent } from 'ngx-placeholder';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxPlaceholderComponent, MatGridListModule],
+  imports: [
+    RouterOutlet,
+    NgxPlaceholderComponent,
+    MatGridListModule,
+    MatDividerModule,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
