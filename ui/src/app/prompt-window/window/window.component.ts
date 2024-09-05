190d57caf4437766680284a9be49b06f2d8477dd
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-09-05 11:52:20 +0530

    input chat texts from prompt-window

--- ui/src/app/prompt-window/window/window.component.ts
+++ ui/src/app/prompt-window/window/window.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -11,26 +11,5 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './window.component.scss',
 })
 export class WindowComponent {
-  public chats: any = [
-    {
-      id: 1,
-      text: 'prepare a 4 day travel trip in manali. cover as much as places possible',
-      sender: 'user',
-    },
-    {
-      id: 2,
-      text: 'ok, here is the list. first travel from xyz then to yzx. first travel from xyz then to yzx. first travel from xyz then to yzx. first travel from xyz then to yzx.',
-      sender: 'ai',
-    },
-    {
-      id: 1,
-      text: 'prepare a 4 day travel trip in manali. cover as much as places possible',
-      sender: 'user',
-    },
-    {
-      id: 2,
-      text: 'ok, here is the list. first travel from xyz then to yzx. first travel from xyz then to yzx. first travel from xyz then to yzx. first travel from xyz then to yzx.',
-      sender: 'ai',
-    },
-  ];
+  @Input() public chats: any[] = [];
 }
